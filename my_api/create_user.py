95401c5e85af90b0703a75beaffa601930a473e3
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-02-19 14:04:11 +0100

    JWT token system

--- my_api/create_user.py
+++ my_api/create_user.py
@@ -1,17 +1,138 @@
-from fastapi import FastAPI, Request
-from sqlmodel import Field, Session, SQLModel, create_engine
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from fastapi import FastAPI, Request, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from sqlmodel import Field, Session, SQLModel, create_engine,select
 from .models import users
-from auth_token import read_items
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from jwt.exceptions import InvalidTokenError
+
+
+SECRET_KEY = "464df42a08b70ffc06bd35be0400953722586f50201dd9ddeb41053ccd837ac1"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 5
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: str | None = None
+
 app = FastAPI()
 
 engine = create_engine("sqlite:///database.db")
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# region python functions
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(username: str):
+    with Session(engine) as session:
+        user = session.exec(select(users).where(users.username == username)).one_or_none()
+    
+    return user
+
+def authenticate_user(username:str, password: str):
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+        
+    user = get_user(token_data.username)
+    if not user:
+        raise credentials_exception
+    return user
+
+async def get_current_active_user(
+    current_user : Annotated[users, Depends(get_current_user)]
+):   
+    print(current_user)
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# region endpoints
+
+@app.get("/users/me")
+async def read_users_me(current_user: Annotated[users, Depends(get_current_active_user)]):
+    
+    return current_user
 
 @app.post("/users/create")
 async def create_user(data:dict):
-    new_user=users(username=data['username'],age=data['age'])
+    new_user=users(**data)
+    new_user.hashed_password = pwd_context.hash(new_user.hashed_password)
     with Session(engine) as session:
         session.add(new_user)
         session.commit()
     return{'message':'Utilisateur bien ajouté.'}
+
+@app.get("/items/")
+async def read_items(token : Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+@app.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )    
+    return Token(access_token=access_token, token_type="bearer")
+
+# @app.post("/token")
+# async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+#     user = get_user(form_data.username)
+#     if not user:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+#     hashed_password = fake_hash_password(form_data.password)
+#     if not hashed_password == user.hashed_password:
+#         raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+#     return {"access_token": user.username, "token_type": "bearer"}
+    
--- my_api/models.py
+++ my_api/models.py
@@ -6,7 +6,10 @@ class users(SQLModel, table=True):
     __tablename__ = "Users"
     id : Optional[int] = Field(default=None, primary_key=True)
     username : str
-    age: int
+    age: Optional[int]
+    hashed_password: str = Field(default="")
+    email: Optional[str]
+    disabled: Optional[bool] = Field(default=False)
 
 # class loan_requests(SQLModel):
 #     pass
