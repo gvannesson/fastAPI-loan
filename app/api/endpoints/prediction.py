85ebc76e53a35140829524085eea0781239b11b7
gvannesson <g.vannesson@gmail.com>
2025-02-21 12:18:44 +0100

    fast api fully terminated

--- app/api/endpoints/prediction.py
+++ app/api/endpoints/prediction.py
@@ -4,7 +4,11 @@ from app.schemas.features import Features
 import pandas as pd
 from app.db.transactions import new_loan_request
 from typing import Annotated
-from app.api.endpoints.users import get_current_user_id
+from app.api.endpoints.users import get_current_user_id, get_current_active_user
+from app.models.loan_requests import Loan_Request
+from app.models.user import User
+from app.db.session import Session, get_session
+from sqlmodel import select
 
 router = APIRouter()
 
@@ -23,3 +27,12 @@ async def predict(features: Features, user_id: Annotated[int, Depends(get_curren
         )    
     new_loan_request(result=result.tolist()[0], user_id=user_id)
     return result.tolist()
+
+
+@router.get("/loans/history")
+async def prediction_history(user: Annotated[User, Depends(get_current_active_user)],user_id: Annotated[User, Depends(get_current_user_id)], session : Annotated[Session, Depends(get_session)]):
+    if user.role != 0:
+        prediction_list = session.exec(select(Loan_Request)).all()
+    else:
+        prediction_list = session.exec(select(Loan_Request).where(Loan_Request.user_id == user_id)).all()
+    return prediction_list
