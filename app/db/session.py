4a7f3c0a133d6f3ce5941ce4ec429dcd6cc86ca8
Samuel Thorez-Debrucq <S.Thorez@hotmail.fr>
2025-03-05 16:21:38 +0100

    formation

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,15 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import create_engine, Session, SQLModel
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///app/db/database.db"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
     with Session(engine) as session:
         yield session
+        
+def init_db():
+    SQLModel.metadata.create_all(engine)
