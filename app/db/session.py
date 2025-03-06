212024a1dbc397e3fd80c6391caabc92870114bf
hacene.zerrouk@gmail.com <hacene.zerrouk@gmail.com>
2025-03-06 12:49:19 +0100

    bdd push sur azure

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,11 @@
 from sqlmodel import create_engine, Session
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///app/db/database.db"
+load_dotenv()
+print (os.getenv('DATABASE_URL'))
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(os.getenv('DATABASE_URL'), echo=True)
 
 def get_session():
     with Session(engine) as session:
